21d712acee7c9bb5c1d0df8739d27f9b8ecdb4b0
pauladanielafarias <pauladanielafarias@gmail.com>
2023-10-02 05:33:02 -0300

    add links

--- src/components/Menu/Navbar.jsx
+++ src/components/Menu/Navbar.jsx
@@ -1,13 +1,26 @@
-import SearchBar from "./SearchBar"
-import "./Navbar.css"
+import SearchBar from "./SearchBar";
+import "./Navbar.css";
+import { Link } from "react-router-dom";
+import PATH_ROUTES from "../../helpers/pathRoutes";
 
 const Navbar = (props) => {
-    const { onSearch } = props;
-    return (
-        <div className="navbar">
-        <SearchBar onSearch={onSearch} />
-      </div>
-    );
-}
+  const { onSearch } = props;
+  return (
+    <div className="navbar">
+      <Link to={PATH_ROUTES.INDEX}>
+        <img
+          className="logo"
+          src="/logo.jpg"
+          alt="Rick and Morty"
+        />
+      </Link>
+
+      <Link to={PATH_ROUTES.HOME}>Home</Link>
+      <Link to={PATH_ROUTES.ABOUT}>About</Link>
+
+      <SearchBar onSearch={onSearch} />
+    </div>
+  );
+};
 
 export default Navbar;
