4269f37a499a8512d47f10bbcfc5f448bafeeb1e
pauladanielafarias <pauladanielafarias@gmail.com>
2023-10-02 05:51:38 -0300

    add text

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,7 +1,23 @@
+import "./About.css";
+
 const About = () => {
   return (
-    <div>
+    <div className="about">
       <h1 className="title">About</h1>
+      <p className="text">
+        Esta es una aplicación creada por <strong>Paula Farias</strong> como trabajo práctico integrador del módulo 2 de la carrera de Desarrollo Web Avanzado de Henry.
+      </p>
+      <p className="text">
+        Se trata de una SPA (Single Page Application) que consume la API pública de Rick and Morty y permite buscar personajes por id.
+      </p>
+      <p className="text">
+        La aplicación fue creada con React, React Router, Axios y CSS.
+      </p>
+      <p className="text">
+        Para ver el código fuente, visitá el <a href="github.com/pauladanielafarias">repositorio de GitHub</a>.
+      </p>
+
+
     </div>
   );
 };
