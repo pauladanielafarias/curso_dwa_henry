482e158d7f2629c5feaed22c1c36ad1bb4d1b1a9
pauladanielafarias <pauladanielafarias@gmail.com>
2023-10-02 05:31:37 -0300

    create Detail page

---
+++ src/components/Detail/Detail.jsx
@@ -0,0 +1,58 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import "./Detail.css";
+
+const Detail = () => {
+  const {id} = useParams();
+  const [character, setCharacter] = useState({});
+
+  useEffect(() => {
+    const getCharacter = async () => {
+      try {
+        const { data } = await axios({
+          method: "GET",
+          url: `https://rickandmortyapi.com/api/character/${id}`,
+        });
+        if (data.id) {
+          setCharacter(data);
+        } else {
+          window.alert("¡No hay personajes con el id ingresado!");
+        }
+      } catch (error) {
+        if (error.response.status === 404) {
+          window.alert("¡No hay personajes con el id ingresado!");
+        }
+        if (error.response.status === 500) {
+          window.alert("Ingresaste un id inválido. ¡Inténtalo de nuevo!");
+        }
+      }
+    };
+    getCharacter();
+    return setCharacter({});
+  }, [id]);
+
+  return (
+    <div>
+      <h1 className="title">Detail</h1>
+
+      <div className="detail container">
+        <div className="left">
+          <h1>{character.name}</h1>
+          <p>STATUS | {character.status} </p>
+          <p>GENDER | {character.gender}</p>
+          <p>SPECIE | {character.species}</p>
+          <p>ORIGIN | {character.origin?.name}</p>
+        </div>
+        <div className="right">
+          <img src={character.image} alt="" />
+        </div>
+
+      </div>
+
+
+    </div>
+  );
+};
+
+export default Detail;
