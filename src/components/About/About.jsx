6cfd2fbecdccb40b182f021c75f8e4d293f03e1d
pauladanielafarias <pauladanielafarias@gmail.com>
2023-10-07 21:23:50 -0300

    change github repo url

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -5,19 +5,28 @@ const About = () => {
     <div className="about">
       <h1 className="title">About</h1>
       <p className="text">
-        Esta es una aplicación creada por <strong>Paula Farias</strong> como trabajo práctico integrador del módulo 2 de la carrera de Desarrollo Web Avanzado de Henry.
+        Esta es una aplicación creada por <strong>Paula Farias</strong> como
+        trabajo práctico integrador del módulo 2 de la carrera de Desarrollo Web
+        Avanzado de Henry.
       </p>
       <p className="text">
-        Se trata de una SPA (Single Page Application) que consume la API pública de Rick and Morty y permite buscar personajes por id.
+        Se trata de una SPA (Single Page Application) que consume la API pública
+        de Rick and Morty y permite buscar personajes por id.
       </p>
       <p className="text">
         La aplicación fue creada con React, React Router, Axios y CSS.
       </p>
       <p className="text">
-        Para ver el código fuente, visitá el <a href="github.com/pauladanielafarias">repositorio de GitHub</a>.
+        Para ver el código fuente, visitá el{" "}
+        <a
+          href="https://github.com/pauladanielafarias/curso_dwa_henry"
+          target="_blank"
+          rel="noreferrer"
+        >
+          repositorio de GitHub
+        </a>
+        .
       </p>
-
-
     </div>
   );
 };
