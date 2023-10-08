82edb3806ff86791ea9d9973feaae7df4ed78da6
pauladanielafarias <pauladanielafarias@gmail.com>
2023-10-07 21:58:44 -0300

    add Form

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,11 @@
+import Form from "../Form/Form";
 import "./Home.css";
 
 const Home = () => {    
   return (
     <div className="home">
-        <h1 className="title">Bienvenidos a Rick and Morty App</h1>
+      <h1 className="title">Bienvenidos a Rick and Morty App</h1>
+      <Form></Form>
     </div>
   );
 };
