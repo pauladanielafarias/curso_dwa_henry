87cc5d85ef724754c0605d686231a5404271a81a
pauladanielafarias <pauladanielafarias@gmail.com>
2023-10-09 20:49:56 -0300

    change logo route and add use location for searchbar

--- src/components/Menu/Navbar.jsx
+++ src/components/Menu/Navbar.jsx
@@ -1,6 +1,6 @@
 import SearchBar from "./SearchBar";
 import "./Navbar.css";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import PATH_ROUTES from "../../helpers/pathRoutes";
 
 const Navbar = (props) => {
@@ -8,17 +8,15 @@ const Navbar = (props) => {
   return (
     <div className="navbar">
       <Link to={PATH_ROUTES.INDEX}>
-        <img
-          className="logo"
-          src="/logo.jpg"
-          alt="Rick and Morty"
-        />
+        <img className="logo" src="logo.jpg" alt="Rick and Morty" />
       </Link>
 
       <Link to={PATH_ROUTES.HOME}>Home</Link>
       <Link to={PATH_ROUTES.ABOUT}>About</Link>
 
-      <SearchBar onSearch={onSearch} />
+      {useLocation().pathname === PATH_ROUTES.HOME && (
+        <SearchBar onSearch={onSearch} />
+      )}
     </div>
   );
 };
