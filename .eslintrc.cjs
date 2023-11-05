485f7d8a45dcfbcec1eb125ad1cc02ef89ab9e5e
pauladanielafarias <pauladanielafarias@gmail.com>
2023-11-05 20:44:47 -0300

    changes in eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,13 +1,13 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2020: true , node: true},
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'node_modules', 'package.json'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
@@ -18,5 +18,6 @@ module.exports = {
     ],
     'react/prop-types': 'off', //elimina el warning de que no se definieron los propTypes
     'no-unused-vars': 'off', //elimina el warning de que no se usaron las variables
+    
   },
 }
