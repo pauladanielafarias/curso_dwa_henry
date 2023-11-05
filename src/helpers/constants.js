fb40ef545cb2a53e79da79e59eabaa84af948999
pauladanielafarias <pauladanielafarias@gmail.com>
2023-11-05 20:50:52 -0300

    add constants

--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -1,6 +1,8 @@
 const constants = {
     BACK_URL: 'http://localhost:8080',
     FRONT_URL: 'http://localhost:3000',
+    LOGO_PATH: '/assets/img/logo.jpg',
+    BACKGROUND_PATH: '/assets/img/background.jpg',
 }
 
 export default constants;
