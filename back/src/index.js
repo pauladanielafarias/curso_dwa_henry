acb7e0bce9729294c717e77547e8ab1455e12e06
pauladanielafarias <pauladanielafarias@gmail.com>
2023-11-05 20:45:15 -0300

    add db connection

--- back/src/index.js
+++ back/src/index.js
@@ -1,3 +1,12 @@
 const { server } = require('./server');
+const { conn } = require('./services/DB_connection');
 
+conn.sync({ force: true })
+    .then(() => {
+    console.log('DB connection successful');
+    })
+    .catch((err) => {
+    console.log('DB connection error', err);
+    }
+);
 server();
