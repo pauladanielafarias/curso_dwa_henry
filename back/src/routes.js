096d2fe97cbd50062282622e87093a8a3855e09e
pauladanielafarias <pauladanielafarias@gmail.com>
2023-11-05 20:44:28 -0300

    add routes

--- back/src/routes.js
+++ back/src/routes.js
@@ -1,5 +1,8 @@
 const router = require('express').Router();
+const { addFavoriteCharacter } = require('./controllers/favoriteCharacters');
 const { getCharacterById } = require('./controllers/characterController');
+const { createUser } = require('./controllers/createUser');
+const { loginUser } = require('./controllers/loginController');
 
 const routes = () => {
 
@@ -7,7 +10,14 @@ const routes = () => {
         res.status(200).send('Hello World');
     });
     
+
     router.get('/character/:id', getCharacterById);
+
+    router.post('/login', loginUser)
+
+    router.post('/signup', createUser);
+
+    router.post('/addCharacter', addFavoriteCharacter);
 
     router.get('*', (req, res) => {
         res.status(404).send('Error 404 Not found');
