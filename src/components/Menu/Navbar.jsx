75576f971bc29cb4628d887cde020db6195e4c48
pauladanielafarias <pauladanielafarias@gmail.com>
2023-11-05 20:50:17 -0300

    general fixes

--- src/components/Menu/Navbar.jsx
+++ src/components/Menu/Navbar.jsx
@@ -2,13 +2,14 @@ import SearchBar from "./SearchBar";
 import "./Navbar.css";
 import { Link, useLocation } from "react-router-dom";
 import PATH_ROUTES from "../../helpers/pathRoutes";
+import constants from "../../helpers/constants";
 
 const Navbar = (props) => {
   const { onSearch } = props;
   return (
     <div className="navbar">
       <Link to={PATH_ROUTES.INDEX}>
-        <img className="logo" src="/logo.jpg" alt="Rick and Morty" />
+        <img className="logo" src={constants.LOGO_PATH} alt="Rick and Morty" />
       </Link>
 
       <Link to={PATH_ROUTES.HOME}>Home</Link>
